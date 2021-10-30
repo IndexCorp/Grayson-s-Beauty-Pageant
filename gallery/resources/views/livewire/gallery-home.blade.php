6d938fc5b9e61504c9c5c5fcd1d2147de8732340
{{Prog.Habeeb}} <olaitan6201@gmail.com>
2021-10-30 11:02:26 +0100

    Modified Gallery Home Page

--- gallery/resources/views/livewire/gallery-home.blade.php
+++ gallery/resources/views/livewire/gallery-home.blade.php
@@ -1,3 +1,26 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
-</div>
+<section class="mt-4 mb-5">
+    <x-home-header></x-home-header>
+    <div class="container-fluid">
+    	<div class="row">
+    		<div class="card-columns justify-content-center align-items-center">
+                @foreach(\App\Models\Gallery::latest()->get() as $gallery)
+                <div class="card card-pin">
+                    @if($gallery->type == 1)
+    				<img class="card-img" src="{{env('FIle_URL').'/gallery/'.$gallery->url}}" alt="{{$gallery->title}}">
+                    @else
+                    <video controls="" autoplay="" name="media"><source src="{{$gallery->url}}" type="video/mp4"></video>
+                    @endif
+    				<div class="overlay">
+    					<h2 class="card-title title">{{$gallery->title}}</h2>
+    					<div class="more">
+    						<a href="{{route('gallery', ['slug'=>$gallery->slug])}}">
+    						<i class="fa fa-arrow-circle-o-right" aria-hidden="true"></i> More </a>
+    					</div>
+    				</div>
+    			</div>
+                @endforeach
+    			
+    		</div>
+    	</div>
+    </div>
+</section>
